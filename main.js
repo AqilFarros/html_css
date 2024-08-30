4969a596c27ccf49f676050ec5658a84c2c578dd
AqilFarros <aqilghonim@gmail.com>
2024-08-30 20:43:37 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const toggleNavbar = () => {
+    document.getElementById("navigation").classList.toggle('active');
+}
+
+const toggleSidebar = () => {
+    document.getElementById("sidebar").classList.toggle('active');
+}
